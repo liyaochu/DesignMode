64afed41ff42f5b60e077698c0672777960183d5
liyaochu <li_yaochu@163.com>
2019-02-26 11:58:12 +0800

    简单工厂模式,注意开闭原则

---
+++ design_pattern/src/com/company/lyc/simplefactory/Video.java
@@ -0,0 +1,11 @@
+package com.company.lyc.simplefactory;
+
+/**
+ * @Auther: Jhon Li
+ * @Date: 2019/2/26 11:33
+ * @Description: 视频类
+ */
+public abstract class Video {
+    //抽象生产方法
+    public abstract  void produce();
+}
