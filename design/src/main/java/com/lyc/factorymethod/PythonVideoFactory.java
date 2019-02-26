fb3cc64b6019f4d0042e502dedc484391730f294
liyaochu <li_yaochu@163.com>
2019-02-26 15:07:27 +0800

    工厂方法父类由子类实现

---
+++ design/src/main/java/com/lyc/factorymethod/PythonVideoFactory.java
@@ -0,0 +1,13 @@
+package com.lyc.factorymethod;
+
+/**
+ * @Auther: Jhon Li
+ * @Date: 2019/2/26 14:00
+ * @Description:
+ */
+public class PythonVideoFactory extends  VideoFactory {
+    @Override
+    public Video getVideo() {
+        return new PythonVideo();
+    }
+}
