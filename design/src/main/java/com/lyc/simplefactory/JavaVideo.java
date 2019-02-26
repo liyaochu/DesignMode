42dea9adfefdcc0d6a94ba7c3d381437c7e34f35
liyaochu <li_yaochu@163.com>
2019-02-26 12:04:52 +0800

    简单工厂模式

---
+++ design/src/main/java/com/lyc/simplefactory/JavaVideo.java
@@ -0,0 +1,13 @@
+package com.lyc.simplefactory;
+
+/**
+ * @Auther: Jhon Li
+ * @Date: 2019/2/26 11:34
+ * @Description: java的视频类
+ */
+public class JavaVideo extends Video {
+    @Override
+    public void produce() {
+        System.out.println("播放java的视频");
+    }
+}
