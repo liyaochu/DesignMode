6d8b78e8680ff3196f0ddfa43def8f469a1cf2b6
liyaochu <li_yaochu@163.com>
2019-03-13 12:18:19 +0800

    策略模式很重要

--- design/src/main/java/com/lyc/behavioral/strategy/service/ProductService.java
+++ design/src/main/java/com/lyc/behavioral/strategy/service/ProductService.java
@@ -3,7 +3,7 @@ package com.lyc.behavioral.strategy.service;
 /**
  * @Auther: Jhon Li
  * @Date: 2019/3/13 11:56
- * @Description:
+ * @Description: 这是自己做的一个简单的案例
  */
 public interface ProductService {
     void produce(String key);
